dcb1c4dc08642f4c5bb2036ba245a83cb744c432
David Obando <daobando@microsoft.com>
2019-05-21 21:45:57 -0700

    Basic project initialization

---
+++ src/gsc/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="David Obando">
+// Copyright (C) GSharp Authors. All rights reserved.
+// </copyright>
+
+namespace Gsc
+{
+    using System;
+
+    /// <summary>
+    /// Entry point to gsc.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Entry point to gsc.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
