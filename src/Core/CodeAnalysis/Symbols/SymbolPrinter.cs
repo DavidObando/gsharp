a8806a4c570cb41a0f7ce4fb7444830a2166ade5
David Obando <daobando@microsoft.com>
2019-06-19 09:28:09 -0700

    Fix bounding of for ellipsis statement

--- src/Core/CodeAnalysis/Symbols/SymbolPrinter.cs
+++ src/Core/CodeAnalysis/Symbols/SymbolPrinter.cs
@@ -6,6 +6,7 @@ namespace GSharp.Core.CodeAnalysis.Symbols
 {
     using System;
     using System.IO;
+    using GSharp.Core.CodeAnalysis.Syntax;
     using GSharp.Core.IO;
 
     /// <summary>
@@ -22,11 +23,84 @@ namespace GSharp.Core.CodeAnalysis.Symbols
         {
             switch (symbol.Kind)
             {
+                case SymbolKind.Function:
+                    WriteFunctionTo((FunctionSymbol)symbol, writer);
+                    break;
+                case SymbolKind.GlobalVariable:
+                    WriteGlobalVariableTo((GlobalVariableSymbol)symbol, writer);
+                    break;
+                case SymbolKind.LocalVariable:
+                    WriteLocalVariableTo((LocalVariableSymbol)symbol, writer);
+                    break;
+                case SymbolKind.Parameter:
+                    WriteParameterTo((ParameterSymbol)symbol, writer);
+                    break;
                 case SymbolKind.Type:
                     WriteTypeTo((TypeSymbol)symbol, writer);
                     break;
                 default:
                     throw new Exception($"Unexpected symbol: {symbol.Kind}");
+            }
+        }
+
+        private static void WriteFunctionTo(FunctionSymbol symbol, TextWriter writer)
+        {
+            writer.WriteKeyword(SyntaxKind.FuncKeyword);
+            writer.WriteSpace();
+            writer.WriteIdentifier(symbol.Name);
+            writer.WritePunctuation(SyntaxKind.OpenParenthesisToken);
+
+            for (int i = 0; i < symbol.Parameters.Length; i++)
+            {
+                if (i > 0)
+                {
+                    writer.WritePunctuation(SyntaxKind.CommaToken);
+                    writer.WriteSpace();
+                }
+
+                symbol.Parameters[i].WriteTo(writer);
+            }
+
+            writer.WritePunctuation(SyntaxKind.CloseParenthesisToken);
+            if (symbol.Type != null)
+            {
+                writer.WriteSpace();
+                writer.WriteIdentifier(symbol.Type.Name);
+            }
+        }
+
+        private static void WriteGlobalVariableTo(GlobalVariableSymbol symbol, TextWriter writer)
+        {
+            writer.WriteKeyword(symbol.IsReadOnly ? SyntaxKind.ConstKeyword : SyntaxKind.VarKeyword);
+            writer.WriteSpace();
+            writer.WriteIdentifier(symbol.Name);
+            if (symbol.Type != null)
+            {
+                writer.WriteSpace();
+                symbol.Type.WriteTo(writer);
+            }
+        }
+
+        private static void WriteLocalVariableTo(LocalVariableSymbol symbol, TextWriter writer)
+        {
+            writer.WriteKeyword(symbol.IsReadOnly ? SyntaxKind.ConstKeyword : SyntaxKind.VarKeyword);
+            writer.WriteSpace();
+            writer.WriteIdentifier(symbol.Name);
+
+            if (symbol.Type != null)
+            {
+                writer.WriteSpace();
+                symbol.Type.WriteTo(writer);
+            }
+        }
+
+        private static void WriteParameterTo(ParameterSymbol symbol, TextWriter writer)
+        {
+            writer.WriteIdentifier(symbol.Name);
+            if (symbol.Type != null)
+            {
+                writer.WriteSpace();
+                symbol.Type.WriteTo(writer);
             }
         }
 
