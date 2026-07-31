189ccf3cdb5bdc0782f7beb102f4ff4caeaf916d
David Obando <davidobando@github.com>
2026-07-31 15:26:58 -0700

    Preserve async select receive bindings

---
+++ test/Interpreter.Tests/Issue2933AsyncSelectArmBindingInterpreterTests.cs
@@ -0,0 +1,43 @@
+// <copyright file="Issue2933AsyncSelectArmBindingInterpreterTests.cs" company="GSharp">
+// Copyright (C) GSharp Authors. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+using GSharp.Core.CodeAnalysis.Compilation;
+using GSharp.Core.CodeAnalysis.Symbols;
+using GSharp.Core.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace GSharp.Interpreter.Tests;
+
+/// <summary>
+/// Issue #2933: interpreter and emitted async select receive bindings agree.
+/// </summary>
+public class Issue2933AsyncSelectArmBindingInterpreterTests
+{
+    [Fact]
+    public void InterpreterPreservesAsyncSelectReceiveBinding()
+    {
+        const string Source = """
+            import Gsharp.Extensions.Go
+
+            async func Run() int32 {
+                let ch = make(chan int32, 1)
+                ch <- 3
+                var result = 0
+                select {
+                    case let value = <-ch { result = 7 + value }
+                }
+                return result
+            }
+
+            Run().GetAwaiter().GetResult()
+            """;
+
+        var result = new Compilation(SyntaxTree.Parse(Source))
+            .Evaluate(new Dictionary<VariableSymbol, object>());
+
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(10, result.Value);
+    }
+}
